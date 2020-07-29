6c2d3ba3ce2915970b605405403578a9e4097a17
wm <graczyk.mateusz@interia.pl>
2020-07-29 12:59:13 +0200

    fix: connection v1

--- Scripts/Client.py
+++ Scripts/Client.py
@@ -5,45 +5,80 @@ import socket
 from ConnectingClientPage import ConnectingClientPage
 
 class Client:
+    
+    socket_connection = None
+    cs = None
 
     @classmethod
     def connecting(cls):
 
-        def try_connect(threadName, delay):
-            is_connected = "connected"
-            
-            while True:
-                try:
-                    cs, address = socket_connection.accept()
-                    cs.send(bytes(is_connected.encode("utf-8")))
-                except:
-                    pass
-        
-        def receiv(threadName, delay):
-            while True:
-                try:
-                    receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    receiv_socket.connect((socket.gethostname(), 50000))
-                    
-                    msg = receiv_socket.recv(10)
-                    msg_decoded = msg.decode("utf-8")
-
-                    if msg_decoded == "connected":
-                        ConnectingClientPage._connected()
-
-                except Exception as err:
-                    print(err)
-
         try:
-            socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket_connection.bind((socket.gethostname(), 45000))
-            socket_connection.listen(1)
+            cls.socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cls.socket_connection.bind((socket.gethostname(), 45000))
+            cls.socket_connection.listen(1)
         except Exception as err:
             print(err)
-            socket_connection.close()
+            cls.socket_connection.close()
 
         try:
-            _thread.start_new_thread(try_connect, ("Thread-1", 4,))
-            _thread.start_new_thread(receiv, ("Thread-2", 4,))
+            _thread.start_new_thread(cls.try_connect, ("Thread-1", 4,))
+            _thread.start_new_thread(cls.receiv, ("Thread-2", 4,))
         except:
             print("Error: unable to start thread")
+    
+    @classmethod
+    def try_connect(cls, threadName, delay):
+        is_connected = "connected"
+        
+        while True:
+            try:
+                cls.cs, address = cls.socket_connection.accept()
+                cls.cs.send(bytes(is_connected.encode("utf-8")))
+            except:
+                pass
+    
+    @classmethod
+    def receiv(cls, threadName, delay):
+        is_connected = False
+        
+        while is_connected is False:
+            try:
+                receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                receiv_socket.connect((socket.gethostname(), 50000))
+                
+                msg = receiv_socket.recv(10)
+                msg_decoded = msg.decode("utf-8")
+
+                if msg_decoded == "connected":
+                    ConnectingClientPage._connected()
+                    is_connected = True
+
+            except Exception as err:
+                print(err)
+
+        while True:
+            try:
+                pass
+                    
+            except Exception as err:
+                print(err)
+
+    @classmethod
+    def send(cls, msg):    
+        try:
+            cls.cs, address = cls.socket_connection.accept()
+            cls.cs.send(bytes(msg.encode("utf-8")))
+        except:
+            pass
+
+    @classmethod
+    def send_rock(cls):
+        cls.send("ROCK")
+
+    @classmethod
+    def send_paper(cls):
+        cls.send("PAPER")
+    
+    @classmethod
+    def send_scissors(cls):
+        cls.send("SCISSORS")
--- Scripts/Host.py
+++ Scripts/Host.py
@@ -6,78 +6,72 @@ from ConnectingHostPage import ConnectingHostPage
 
 class Host:
 
+    socket_connection = None
+    cs = None
+    
     @classmethod
     def connecting(cls):
-    
-        def try_connect(threadName, delay):
-            is_connected = "connected"
-            
-            while True:
-                try:
-                    cs, address = socket_connection.accept()
-                    cs.send(bytes(is_connected.encode("utf-8")))
-                except:
-                    pass
-
-        def receiv(threadName, delay):
-            while True:
-                try:
-                    receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    receiv_socket.connect((socket.gethostname(), 45000))
-                    
-                    msg = receiv_socket.recv(10)
-                    msg_decoded = msg.decode("utf-8")
-
-                    if msg_decoded == "connected":
-                        ConnectingHostPage._connected()
-                        #send feedback to client
-                        
-                except Exception as err:
-                    print(err)
-
-            while True:
-                try:
-                    receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    receiv_socket.connect((socket.gethostname(), 45000))
-                    
-                    msg = receiv_socket.recv(10)
-                    msg_decoded = msg.decode("utf-8")
-
-                    if msg_decoded == "ROCK":
-                        pass
-                    elif msg_decoded == "PAPER":
-                        pass
-                    elif msg_decoded == "SCISSORS":
-                        pass
-                        
-                except Exception as err:
-                    print(err)
-
         try:
-            socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket_connection.bind((socket.gethostname(), 50000))
-            socket_connection.listen(1)
+            cls.socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cls.socket_connection.bind((socket.gethostname(), 50000))
+            cls.socket_connection.listen(1)
         except Exception as err:
             print(err)
-            socket_connection.close()
+            cls.socket_connection.close()
 
         try:
-            _thread.start_new_thread(try_connect, ("Thread-3", 4,))
-            _thread.start_new_thread(receiv, ("Thread-4", 4,))
+            _thread.start_new_thread(cls.try_connect, ("Thread-3", 4,))
+            _thread.start_new_thread(cls.receiv, ("Thread-4", 4,))
         except:
             print("Error: unable to start thread")
 
-    @classmethod
-    def send_rock(cls):
-        pass
-
-    @classmethod
-    def send_paper(cls):
-        pass
+    @classmethod       
+    def try_connect(cls, threadName, delay):
+        is_connected = "connected"
+        
+        while True:
+            try:
+                cls.cs, address = cls.socket_connection.accept()
+                cls.cs.send(bytes(is_connected.encode("utf-8")))
+            except:
+                pass
     
     @classmethod
-    def send_scissors(cls):
-        pass
+    def receiv(cls, threadName, delay):
+        is_connected = False
+
+        while is_connected is False:
+            try:
+                receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                receiv_socket.connect((socket.gethostname(), 45000))
+                
+                msg = receiv_socket.recv(10)
+                msg_decoded = msg.decode("utf-8")
+
+                if msg_decoded == "connected":
+                    ConnectingHostPage._connected()
+                    is_connected = True
+
+            except Exception as err:
+                print(err)
+
+        while True:
+            try:
+                receiv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                receiv_socket.connect((socket.gethostname(), 45000))
+                
+                msg = receiv_socket.recv(10)
+                msg_decoded = msg.decode("utf-8")
+                print(msg_decoded)
+                if msg_decoded == "ROCK":
+                    pass
+                elif msg_decoded == "PAPER":
+                    pass
+                elif msg_decoded == "SCISSORS":
+                    pass
+                    
+            except Exception as err:
+                print(err)
 
     @classmethod
     def lose_connection(cls):
